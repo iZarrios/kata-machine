843728855b10df556919e7e4e2a8fce8d2c670ef
iZarrios <zarriose@gmail.com>
2023-07-01 15:09:09 +0300

    add: TwoCrystalBall Problem Implementation

---
+++ src/day1/TwoCrystalBalls.ts
@@ -0,0 +1,15 @@
+export default function two_crystal_balls(breaks: boolean[]): number {
+    const jump = Math.floor(Math.sqrt(breaks.length));
+    for(let i = jump; i < breaks.length;i+=jump) {
+        if (breaks[i]) {
+            for(let j = i - jump; j < i; j++) {
+                if (breaks[j]) {
+                    return j;
+                }
+            }
+        }
+    }
+
+    return -1;
+
+}
